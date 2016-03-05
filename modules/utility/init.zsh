f7b5966ecd71a46e366d9f1c106211bba54f430f
Josh Hamacher <hamacher@brutallogic.net>
2016-03-05 05:39:56 -0800

    Disable noglob for scp and other commands.

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -41,9 +41,9 @@ alias ftp='noglob ftp'
 alias history='noglob history'
 alias locate='noglob locate'
 alias rake='noglob rake'
-alias rsync='noglob rsync'
-alias scp='noglob scp'
-alias sftp='noglob sftp'
+#alias rsync='noglob rsync'
+#alias scp='noglob scp'
+#alias sftp='noglob sftp'
 
 # Define general aliases.
 alias _='sudo'
